a124703ef72ed4e74e64794d15d64737cc1633b7
Graham Dennis <graham.dennis@gmail.com>
2018-01-01 10:33:15 +1100

    Initial commit of building hello world.

---
+++ polog-protobuf/src/main/java/io/grpc/examples/helloworld/HelloWorldClient.java
@@ -0,0 +1,84 @@
+/*
+ * (c) Copyright 2017 Graham Dennis. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.grpc.examples.helloworld;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.StatusRuntimeException;
+import java.util.concurrent.TimeUnit;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * A simple client that requests a greeting from the {@link HelloWorldServer}.
+ */
+public final class HelloWorldClient {
+    private static final Logger log = LoggerFactory.getLogger(HelloWorldClient.class);
+
+    private final ManagedChannel channel;
+    private final GreeterGrpc.GreeterBlockingStub blockingStub;
+
+    /** Construct client connecting to HelloWorld server at {@code host:port}. */
+    public HelloWorldClient(String host, int port) {
+        this(ManagedChannelBuilder.forAddress(host, port)
+                // Channels are secure by default (via SSL/TLS). For the example we disable TLS to avoid
+                // needing certificates.
+                .usePlaintext(true)
+                .build());
+    }
+
+    /** Construct client for accessing RouteGuide server using the existing channel. */
+    HelloWorldClient(ManagedChannel channel) {
+        this.channel = channel;
+        blockingStub = GreeterGrpc.newBlockingStub(channel);
+    }
+
+    public void shutdown() throws InterruptedException {
+        channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+    }
+
+    /** Say hello to server. */
+    public void greet(String name) {
+        log.info("Will try to greet {} ...", name);
+        HelloRequest request = HelloRequest.newBuilder().setName(name).build();
+        HelloReply response;
+        try {
+            response = blockingStub.sayHello(request);
+        } catch (StatusRuntimeException e) {
+            log.warn("RPC failed: {0}", e.getStatus());
+            return;
+        }
+        log.info("Greeting: {}", response.getMessage());
+    }
+
+    /**
+     * Greet server. If provided, the first element of {@code args} is the name to use in the greeting.
+     */
+    public static void main(String[] args) throws Exception {
+        HelloWorldClient client = new HelloWorldClient("localhost", 50051);
+        try {
+      /* Access a service running on the local machine on port 50051 */
+            String user = "world";
+            if (args.length > 0) {
+                user = args[0]; /* Use the arg as the name to greet if provided */
+            }
+            client.greet(user);
+        } finally {
+            client.shutdown();
+        }
+    }
+}
